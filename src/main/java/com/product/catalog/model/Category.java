2a0b0e586f3fededfbda62168bdd59a7afa32f40
Seiha <naingseiha@gmail.com>
2025-07-05 13:16:45 +0200

    Initial Project with model

---
+++ src/main/java/com/product/catalog/model/Category.java
@@ -0,0 +1,18 @@
+package com.product.catalog.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Product> products;
+}
